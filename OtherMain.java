893577265dd597d4006022057b709c53b9253f58
tnduong2807 <tnduong@ucsd.edu>
2022-09-30 20:45:25 -0700

    lab-report1

---
+++ OtherMain.java
@@ -0,0 +1,16 @@
+class WhereAmI {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println("New editing, Neet");
+        System.out.println("Optimise editing");
+
+
+    }
+
+
+
+
+}
